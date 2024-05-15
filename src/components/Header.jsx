fb4b6c081bdb4835c166c6f7b2a7c55bf8654850
lgadol <pedro.gado03@gmail.com>
2024-05-15 18:00:13 -0300

    header ajust

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import '../style/Header.css'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { FaCrown, FaUser, FaSignInAlt, FaShoppingCart, FaStore } from 'react-icons/fa';
+import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { FaCrown, FaUser, FaSignInAlt, FaShoppingCart, FaStore, FaSignOutAlt } from 'react-icons/fa';
 
 export const Header = () => {
     const [userName, setUserName] = useState(localStorage.getItem('userName'));
     const isAdmin = localStorage.getItem('isAdmin');
+    const history = useHistory();
 
     useEffect(() => {
         setUserName(localStorage.getItem('userName'));
     }, []);
+
+    const handleLogout = () => {
+        localStorage.clear();
+        setUserName(null);
+        history.push('/login');
+    }
 
     return (
         <div className='header_div'>
             <Link to="/"><FaStore /> Loja</Link>
             <Link to="/cart"><FaShoppingCart /> Carrinho</Link>
             {userName ? (
-                <p className='header_p'>
-                    Bem-vindo
-                    <br />
-                    {isAdmin === '1' ? <FaCrown color='yellow' /> : <FaUser color='white' />}
-                    {" "} <Link to="/profile">{userName}</Link>
-                </p>
+                <>
+                    <p className='header_p'>
+                        Bem-vindo
+                        <br />
+                        {isAdmin === '1' ? <FaCrown color='yellow' /> : <FaUser color='white' />}
+                        {" "} <Link to="/profile">{userName}</Link>
+                    </p>
+                    <a className='exit_p' onClick={handleLogout}><FaSignOutAlt color='red'/> Sair</a>
+                </>
             ) : (
                 <Link to="/login"><FaSignInAlt /> Entrar</Link>
             )}
